385ac8a9ca5fcc1ea7fea4380ef7585d0bab9969
Othallia <vianiputriocta6@gmail.com>
2024-04-04 17:59:03 +0700

    menambahkan input while

--- AlgorithmaSearch_0032/AlgorithmaSearch_0032.cpp
+++ AlgorithmaSearch_0032/AlgorithmaSearch_0032.cpp
@@ -5,4 +5,8 @@ int arr[20]; // Array to be searched
 int n; // number of elements in the array
 int i; // index of array element
 
-
+void input() {
+	while (true) {
+		cout << "Enter the number of element in the array: ";
+		cin >> n;
+	}
