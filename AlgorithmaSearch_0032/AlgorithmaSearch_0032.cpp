2e1ab56bce168c53dde49bf04d5252ad6a227b72
Othallia <vianiputriocta6@gmail.com>
2024-04-04 18:14:38 +0700

    menamabahkan input, linearsearch, dan display

--- AlgorithmaSearch_0032/AlgorithmaSearch_0032.cpp
+++ AlgorithmaSearch_0032/AlgorithmaSearch_0032.cpp
@@ -36,3 +36,41 @@ void LinearSearch() {
 		cout << "\nEnter the element you want to search: ";
 		cin >> item;
 
+		ctr = 0;
+		i = 0; //step 2
+		while (i < n) // step 3
+		{
+			ctr++;
+			if (arr[i] == item) {
+				cout << "\n" << item << " fount at position " << (i + 1) << endl; // step 6
+				break;
+			}
+			i++;  // step 4
+		}
+
+		if (i == n) //step 5
+			cout << "\n" << item << " not found in the array\n";
+		cout << "\nNumber of comparisons: " << ctr << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+
+	} while ((ch == 'y') || (ch == 'Y'));
+}
+
+void display()
+{
+	cout << "Array element are: ";
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+	input();
+	LinearSearch();
+	display();
+}
+
+
